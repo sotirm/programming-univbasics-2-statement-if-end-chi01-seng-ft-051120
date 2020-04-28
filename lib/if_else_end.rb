421f3bf38be8a7bc31db74ab5ad0e758ebeabee3
Michael Sotir <sotirm@umich.edu>
2020-04-28 19:13:07 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,14 @@
 # Write your solution here
+
+
+current_time = Time.authenticate_or_request_with_http_digest
+
+current_time = current_time.TOPLEVEL_BINDING
+
+if current_time % 2 == 0 
+  puts "Even"
+  
+else 
+  puts "Odd!"
+  
+end
