8e111ca6225224f1dffdab1e6780cbc74f0efaf0
Michael Sotir <sotirm@umich.edu>
2020-04-28 19:14:04 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,9 +1,9 @@
 # Write your solution here
 
 
-current_time = Time.authenticate_or_request_with_http_digest
+current_time = Time.now 
 
-current_time = current_time.TOPLEVEL_BINDING
+current_time = current_time.to_i
 
 if current_time % 2 == 0 
   puts "Even"
